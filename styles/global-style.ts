a12fb1f1bba82f1f3d4a8924a4bb98e48364ef0b
tpgus <tpgus758@naver.com>
2023-01-06 09:24:14 +0900

    init

--- styles/global-style.ts
+++ styles/global-style.ts
@@ -6,14 +6,14 @@ export const GlobalStyle = createGlobalStyle`
 //reset은 styled-component를 사용하며, 여러 브라우저마다 기본적으로 적용된 스타일을 지워주는 Node.js 패키지 : 스타일 초기화
     ${reset}
     body {
-         box-sizing: border-box;
-         margin: 0;
+        box-sizing: border-box;
+        margin: 0;
         padding: 0;
     }
 
-    ::-webkit-scrollbar {
-        display: overlay;
-    }
+    /* ::-webkit-scrollbar {
+        display: none;
+    } */
 
     button {
         padding: 0;
