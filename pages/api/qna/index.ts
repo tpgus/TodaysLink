ba8e1b5ca7821ee54ce995bdd5536c87ed836862
tpgus <tpgus758@naver.com>
2023-01-24 10:05:22 +0900

    테스트

--- pages/api/qna/index.ts
+++ pages/api/qna/index.ts
@@ -53,7 +53,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       res.status(500).json({ message: "내 문의 조회 실패" });
     }
   }
-  client.close();
+  //   client.close();
 };
 
 export default handler;
