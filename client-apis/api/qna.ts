c6de5e5b5bb77047d1a46e346e8f58213c131a66
tpgus <tpgus758@naver.com>
2023-02-06 18:15:48 +0900

    수정

--- client-apis/api/qna.ts
+++ client-apis/api/qna.ts
@@ -40,7 +40,7 @@ export const createQnA = async (qna: any) => {
 };
 
 export const getQnAList = async () => {
-  console.log(process.env.apiKey);
+  console.log(process.env.NEXT_PUBLIC_apiKey);
   console.log(process.env.NODE_ENV);
   try {
     const qnaRef = getRef();
