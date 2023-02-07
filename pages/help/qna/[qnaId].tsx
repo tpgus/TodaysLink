5398ec52399c747e95b7c8f7fcc9813b63cc342e
tpgus <tpgus758@naver.com>
2023-02-07 18:38:32 +0900

    qna 디테일 페이지 - firebase

--- pages/help/qna/[qnaId].tsx
+++ pages/help/qna/[qnaId].tsx
@@ -3,25 +3,27 @@ import HelpPageLayout from "../../../components/help/HelpPageLayout";
 import LoadingSpinner from "../../../components/common/LoadingSpinner";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { getQnAById } from "../../../client-apis/api/qna";
 import type { QnaType } from "../../../types";
 import type { ReactElement } from "react";
+import type { ParsedUrlQuery } from "querystring";
+
+interface Query extends ParsedUrlQuery {
+  qnaId: string;
+}
 
 const QnaDetailPage = () => {
   const router = useRouter();
   const [qna, setQna] = useState<QnaType | null>(null);
   const [isLoading, setIsLoading] = useState(false);
-  const { qnaId } = router.query;
+  const { qnaId } = router.query as Query;
 
   useEffect(() => {
-    if (qnaId) {
-      setIsLoading(true);
-      fetch(`/api/qna/${qnaId}`)
-        .then((response) => response.json())
-        .then((data) => {
-          setIsLoading(false);
-          setQna(data.qnaItem);
-        });
-    }
+    const fetchQnA = async () => {
+      const qna = await getQnAById(qnaId);
+      setQna(qna);
+    };
+    fetchQnA();
   }, [qnaId]);
 
   if (qna === null && isLoading) return <LoadingSpinner />;
